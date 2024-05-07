6ae07f7e55973ec64fb3b671d5f7fd05999ce523
WilliamFarrow <168533664+WilliamFarrow@users.noreply.github.com>
2024-05-07 11:15:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
+const router = require('./routes');
+
 app.get('/', (req, res) => {
   res.send('hello world1')
 })
 
+const router = require('./routes');
+
 app.use("/", router);
+
 var server = app.listen(5000, function () {
    console.log("Express App running at http://127.0.0.1:5000/");
 })
